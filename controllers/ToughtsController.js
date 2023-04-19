9aabddded39256308d1dea738e0bd541f157cece
Alexandre Júnior <alexandrepj13@gmail.com>
2023-04-19 11:07:30 -0300

    finally project

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,9 +1,38 @@
 const Tought = require("../models/Tought")
 const User = require("../models/User")
+const {Op} = require('sequelize')
 
 module.exports = class ToughtController {
     static async showToughts(req, res) {
-        res.render('toughts/home')
+
+        let search = ''
+
+        if(req.query.search){
+            search=req.query.search
+        }
+
+        let order = 'DESC'
+
+        if(req.query.order==='old'){
+            order = 'ASC'
+        } else{
+            order = 'DESC'
+        }
+
+        const toughtsData = await Tought.findAll({
+            include:User,
+            where:{
+                title:{[Op.like]:`%${search}%`}
+            },
+            order:[['createdAt',order]]
+        })
+        const toughts = toughtsData.map((result)=>result.get({plain:true}))
+        let toughtsQty = toughts.length
+        if(toughtsQty===0){
+            toughtsQty=false
+        }
+
+        res.render('toughts/home', {toughts,search,toughtsQty})
     }
 
     static async dashboard(req, res) {
@@ -23,25 +52,48 @@ module.exports = class ToughtController {
 
         res.render('toughts/dashboard',{toughts})
     }
-
     static async createTougthtCreate(req, res) {
         const toughts = {
             title: req.body.title,
             UserId: req.session.userid
         }
-
         try {
             await Tought.create(toughts)
             req.flash('message', 'Pensamento criado com sucesso !')
             req.session.save(() => {
                 res.redirect('/toughts/dashboard')
             })
-
-        } catch (error) {
-            console.log(error)
-        }
+        } catch (error) {console.log(error)}
     }
-
+    static async removeTought(req,res){
+        try {
+            const id = req.body.id
+            const UserId = req.session.userid
+            await Tought.destroy({where:{id:id, UserId:UserId}})
+            req.flash('message', 'Pensamento removido com sucesso !')
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {console.log(error) }
+    }
+    static async updateToughtSave(req,res){
+        try {
+            const id = req.body.id
+            const tought = {
+                title:req.body.title
+            }
+        await Tought.update(tought, {where:{id:id}})
+        req.flash('message', 'Pensamento atualizado com sucesso !')
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+        })  
+        } catch (error) {console.log(error)}
+    }
+    static async updateTought(req,res){
+        const id = req.params.id
+        const tought = await Tought.findOne({raw:true,where:{id:id}})
+        res.render('toughts/edit', {tought})
+    }
     static createTought(req, res) {
         res.render('toughts/create')
     }
--- routers/toughtsRouter.js
+++ routers/toughtsRouter.js
@@ -7,6 +7,8 @@ router.get('/', ToughtController.showToughts)
 router.get('/dashboard',checkAuth, ToughtController.dashboard)
 router.get('/add',checkAuth, ToughtController.createTought)
 router.post('/add', checkAuth, ToughtController.createTougthtCreate)
-
+router.post("/delete", checkAuth, ToughtController.removeTought)
+router.get('/edit/:id', checkAuth, ToughtController.updateTought)
+router.post('/edit', checkAuth, ToughtController.updateToughtSave)
 
 module.exports = router
